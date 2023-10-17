055ebbad25c3040786859f32e979c6c47ec4c624
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-10-18 02:31:30 +0300

    done by editing the

---
+++ Globe-Wander-Final/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,57 @@
+using Globe_Wander_Final.Models.DTOs;
+using Globe_Wander_Final.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Globe_Wander_Final.Pages
+{
+    [ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)] // Disable caching
+    public class DashboardModel : PageModel
+    {
+
+        private readonly ITourSpot _tour;
+        private readonly ITrip _trip;
+        private readonly IHotel _hotel;
+        private readonly IBookingRoom _bookingRoom;
+        private readonly IBookingTrip _bookingTrip;
+        private readonly IRate _rate;
+
+        public DashboardModel(ITourSpot tour, ITrip trip, IHotel hotel, IBookingRoom bookingRoom, IBookingTrip bookingTrip, IRate rate)
+        {
+            _tour = tour;
+            _trip = trip;
+            _hotel = hotel;
+            _bookingRoom = bookingRoom;
+            _bookingTrip = bookingTrip;
+            _rate = rate;
+        }
+
+
+        public int ToursCount { get; set; }
+        public int TripCount { get; set; }
+        public int HotelCount { get; set; }
+        public int BookingsCount { get; set; }
+        public int RateCount { get; set; }
+
+        public async Task OnGet()
+        {
+            var tours  = await _tour.GetAllTourSpots();
+            ToursCount = tours.Count;
+
+            var hotels = await _hotel.GetAllHotels();
+            HotelCount = hotels.Count();
+
+            var trips = await _trip.GetAllTrips();
+            TripCount = trips.Count();
+
+            var bookingRooms = await _bookingRoom.GetAllBookingRooms();
+            var bookingTrips = await _bookingTrip.GetAllBookingTrips();
+
+            BookingsCount = bookingRooms.Count() + bookingTrips.Count();
+
+            var rates = await _rate.GetAllRate();
+            RateCount = rates.Count();
+
+        }
+    }
+}
